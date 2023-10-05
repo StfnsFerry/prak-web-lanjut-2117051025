c27cea9bd76b311efdcc75afcc34d4c635438540
StfnsFerry <stefanusferry.05@gmail.com>
2023-10-05 18:18:38 +0700

    tugas4

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
 
 class UserController extends BaseController
 {
@@ -15,7 +16,7 @@ class UserController extends BaseController
         $data = [
             'nama' => $nama,
             'kelas' => $kelas,
-            'npm' => $npm
+            'npm' => $npm,
         ];
 
         return view('profile',$data);
@@ -23,15 +24,64 @@ class UserController extends BaseController
     }
 
     public function create(){
-        return view('create_user');
+        $kelas = [
+            [
+                'id' => 1,
+                'nama_kelas' => 'A'
+            ],
+            [
+                'id' => 2,
+                'nama_kelas' => 'B'
+            ],
+            [
+                'id' => 3,
+                'nama_kelas' => 'C'
+            ],
+            [
+                'id' => 4,
+                'nama_kelas' => 'D'
+            ],
+        ];
+
+        $data  = [
+            'kelas' => $kelas,
+            'validation' => \Config\Services::validation()
+        ];
+        return view('create_user', $data);
     }
 
     public function store(){
+        if (!$this->validate([
+            'nama' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} mahasiswa harus di isi.'
+                ]
+            ],
+            'npm' => [
+                'rules' => 'required|is_unique[user.npm]',
+                'errors' => [
+                    'required' => '{field} mahasiswa harus di isi.',
+                    'is_unique' => '{field} mahasiswa sudah terdaftar.'
+                ]
+            ]
+        ])) {
+            $validation = \Config\Services::validation();
+            return redirect()->to(base_url('/user/create'))->withInput()->with('validation', $validation);
+        }
+
+        $userModel = new UserModel();
+        $userModel->saveUser([
+            'nama' => $this->request->getVar('nama'),
+            'id_kelas' => $this->request->getVar('kelas'),
+            'npm' => $this->request->getVar('npm'),
+        ]);
+        // dd($this->request->getVar());
         $data = [
             'nama' => $this->request->getVar('nama'),
             'kelas' => $this->request->getVar('kelas'),
             'npm' => $this->request->getVar('npm'),
         ];
-        return view('profile',$data);
+        return view('profile', $data);
     }
 }
