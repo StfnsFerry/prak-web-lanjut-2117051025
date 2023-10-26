548d1394df6a27228c01d8fcf9786de4e0acf2af
StfnsFerry <stefanusferry.05@gmail.com>
2023-10-26 12:31:29 +0700

    update-delete

--- app/Views/list_user.php
+++ app/Views/list_user.php
@@ -3,38 +3,59 @@
 
 <div class="container">
     <div class="list-box">
-        <h1 class="mb-0">Tabel User</h3>
-        <button class="btn btn-dark mb-3"><a href="<?= base_url('/user/create')?>">+ Tambah Data</a></button>
-        <div class="list-table">
-            <table class="table table-hover">
-              <thead>
-                <tr>
-                  <td>No</td>
-                  <td>Nama</td>
-                  <td>NPM</td>
-                  <td>Kelas</td>
-                  <td>Aksi</td>
-                </tr>
-              </thead>
-              <tbody>
-                <?php
-                $no = 1;
-                foreach ($users as $user) { ?>
-                  <tr>
-                    <td><?= $no++ ?></td>
-                    <td><?= $user['nama'] ?></td>
-                    <td><?= $user['npm'] ?></td>
-                    <td><?= $user['nama_kelas'] ?></td>
-                    <td>
-                      <a class="btn btn-primary" href="<?= base_url('user/' . $user['id'])?>">Detail</a>
-                      <a class="btn btn-warning" href="">Edit</a>
-                      <a class="btn btn-danger" href="">Hapus</a>
-                    </td>
-                  </tr>
-                <?php }
-                ?>
-              </tbody>
-            </table>
+        <div class="card shadow mb-4">
+            <div class="card-header py-3 d-flex justify-content-between align-items-center">
+                <h6 class="m-0 font-weight-bold text-dark">List User</h6>
+                <a href="<?= base_url('/user/create')?>" class="btn btn-dark">+ Tambah Data</a>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table
+                    class="table table-bordered"
+                    id="dataTable"
+                    width="100%"
+                    cellspacing="0"
+                  >
+                    <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th>Nama</th>
+                            <th>NPM</th>
+                            <th>Kelas</th>                           
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                      <?php
+                        $no = 1;
+                        foreach($users as $user){
+                          ?>
+                          <tr>
+                            <td><?= $no++ ?></td>
+                            <td><?= $user['nama']?></td>                            
+                            <td><?= $user['npm']?></td>                              
+                            <td><?= $user['nama_kelas']?></td>             
+                            <td>
+                              <div class="wrap-icon">
+                                <a class="btn btn-secondary me-2" href="<?= base_url('user/' . $user['id']) ?>"><i class="fas fa-eye text-dark" aria-hidden="true"></i></a>
+                                <a class="btn btn-dark me-2" href="<?= base_url('user/' . $user['id'] . '/edit') ?>"><i class="fas fa-pencil-alt text-dark" aria-hidden="true"></i></a>
+                                <form action="<?= base_url('user/' . $user['id']) ?>" method="POST">
+                                  <input type="hidden" name="_method" value="DELETE">
+                                  <?= csrf_field() ?>
+                                  <button class="btn btn-danger"">
+                                    <i class="fas fa-trash text-dark" aria-hidden="true"></i>
+                                  </button>
+                                </form>                   
+                              </div>
+                            </td>
+                          </tr>                                                                                  
+                          <?php
+                         }
+                         ?>
+                    </tbody>
+                </table>
+              </div>
+            </div>  
         </div>
     </div>
 </div>
--- app/Views/profile.php
+++ app/Views/profile.php
@@ -1,26 +1,44 @@
 <?= $this->extend('layouts/app')?>
 <?= $this->section('content')?>
-<body>
-    <div class="home">
-        <div class="profile">
-            <div class="box-profile">
-                <div class="img">
-                    <img src="<?= $user['foto'] ?? '<default-foto>'?>">
-                </div>
-                <div class="box-data">
-                    <p class="title">Nama Lengkap</p>
-                    <h1 class="data"><?=$user['nama']?></h1>
-                </div>
-                <div class="box-data">
-                    <p class="title">Kelas</p>
-                    <h1 class="data"><?=$user['id_kelas']?></h1>
-                </div>
-                <div class="box-data">
-                    <p class="title">NPM</p>
-                    <h1 class="data"><?=$user['npm']?></h1>
+    <div class="container d-flex justify-content-center align-items-center min-vh-100">
+        <div class="row border rounded-5 p-3 bg-white shadow box-area">
+            <div class="col-md-6 rounded-4 left-box d-flex justify-content-center align-items-center">
+                <div class="featured-image rounded-4" style="height:350px; width:100%; background-image: url('<?= $user['foto']?>'); background-size: cover;">
+                    
                 </div>
             </div>
-
+            <div class="col-md-6 d-flex justify-content-center align-items-center flex-column">
+                <div class="form-wrap w-100">
+                    <div class="form-header d-flex align-items-center justify-content-between mb-4">
+                        <div class="title">
+                            <h4 class="mb-1">Profile</h4>
+                            <p class="mb-0">Details data</p>
+                        </div>
+                        <div class="back">
+                            <a href="<?= base_url('/user')?>" class="btn btn-outline-dark"><i class="fas fa-arrow-left mb-0"></i></a>
+                        </div>
+                    </div>
+                    <div class="desc">
+                        <div class="desc-content">
+                            <div class="desc-text mb-3">
+                                <p class="mb-2">Nama Lengkap</p>
+                                <h1 class="mb-0"><?= $user['nama']?></h1>
+                            </div>
+                            <div class="desc-text mb-3">
+                                <p class="mb-2">NPM</p>
+                                <h1 class="mb-0"><?= $user['npm']?></h1>
+                            </div>
+                            <div class="desc-text mb-3">
+                                <p class="mb-2">Kelas</p>
+                                <h1 class="mb-0"><?= $user['nama_kelas']?></h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="edit d-flex justify-content-end align-items-center">
+                        <a href="<?= base_url('user/' . $user['id'] . '/edit') ?>" class="btn btn-dark">Edit Profile</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 <?= $this->endSection('content') ?>
